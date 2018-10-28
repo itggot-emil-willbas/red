fd9ac6614ac9830c7731569fc2410a69ae64a304
Emil Willbas <emil@vildbas.com>
2018-10-28 16:38:58 +0100

    3
    3

--- src/components/Article3.vue
+++ src/components/Article3.vue
@@ -1,13 +1,24 @@
 <template>
     <article id="article-3">
         <h2>Article 3</h2>
+        <p 
+            v-for="employee in getEmployees" 
+            v-bind:key="employee.id">
+            {{employee.name}}, {{employee.age}} år, bor i {{employee.city}}
+        </p>
     </article>
     
 </template>
 
 <script>
 export default {
-    name:'Article3'
+    name:'Article3',
+    computed:{
+        getEmployees () {
+            return this.$store.getters.getEmployees;
+            //return this.$store.state.myJson.employees  
+        }
+    }
 }
 </script>
 
@@ -16,11 +27,15 @@ export default {
 @import '../css/style.scss';
 
 #article-3 {
+    overflow-y:scroll;
     background:$vue-white;
     flex-basis:$vue-article-width;
     padding:10px 50px;
     h2 {
         color:$vue-grey;
+    }
+    p {
+        text-align: left;
     }
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import json from './employees.json'
 
 Vue.use(Vuex)
 
@@ -10,12 +11,18 @@ export default new Vuex.Store({
       {id:1,text:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, deleniti.",author:"Emil Willbas"},
       {id:2,text:"Vipsum Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, deleniti.",author:"Atrid Lindgren"},
       {id:3,text:"Ipsum Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, deleniti.",author:"Haruki Murakami"}
-    ]
+    ],
+    myJson:json
+  },
+  getters: {
+    getEmployees: state => {
+      return state.myJson.employees;
+    }
   },
   mutations: {
 
   },
   actions: {
-
+    
   }
 })
